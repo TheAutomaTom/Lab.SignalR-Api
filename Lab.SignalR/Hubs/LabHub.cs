742e7c2b62152f503c6f6f9a08119231722a2693
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-18 10:58:44 -0500

    Add Hub type

---
+++ Lab.SignalR/Hubs/LabHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Lab.SignalR.Hubs
+{
+	public class LabHub : Hub
+	{
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+	}
+}
