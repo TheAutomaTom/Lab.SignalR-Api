f707d9e45899d2600d9b2b704539d696d7890d62
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-18 10:58:53 -0500

    Configure Hubs

--- Lab.SignalR/Program.cs
+++ Lab.SignalR/Program.cs
@@ -1,11 +1,16 @@
+using Lab.SignalR.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSignalR();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -20,6 +25,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+	endpoints.MapHub<LabHub>("/lab-hub"); // Map the SignalR hub
+});
 
 app.Run();
